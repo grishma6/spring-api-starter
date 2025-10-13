5a459034d8ce06ff08af80a65016c5eccf536926
grishma6 <gxg59150@ucmo.edu>
2025-10-13 16:14:05 -0500

    Java objects to JSON objects

---
+++ src/main/java/com/codewithmosh/store/entities/Message.java
@@ -0,0 +1,11 @@
+package com.codewithmosh.store.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+public class Message {
+    private String text;
+}
